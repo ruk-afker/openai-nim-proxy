02c525f548bb068b870348eb3461df721f5899b3
ruk-afker <ryemeadow70@gmail.com>
2026-05-08 07:58:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const ENABLE_THINKING_MODE = false;
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': process.env.MODEL_SMALL  || 'z-ai/glm-5.1',
   'gpt-4':         process.env.MODEL_MID    || 'nvidia/llama-3.3-nemotron-super-49b-v1',
-  'gpt-4-turbo':   process.env.MODEL_LARGE  || 'meta/llama-3.3-70b-instruct',
+  'gpt-4-turbo':   process.env.MODEL_LARGE  || 'deepseek-ai/deepseek-v4-pro',
   'gpt-4o':        process.env.MODEL_BEST   || 'google/gemma-4-31b-it',
   'claude-3-opus': process.env.MODEL_ALT    || 'deepseek-ai/deepseek-v4-flash',
   'o1':            process.env.MODEL_REASON || 'deepseek-ai/deepseek-r1',
