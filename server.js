492199c4380aae9b19f8bc6729cb9467fe351400
ruk-afker <ryemeadow70@gmail.com>
2026-08-19 21:39:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.c
 const NIM_API_KEY = process.env.NIM_API_KEY;
 
 // 🔥 REASONING DISPLAY TOGGLE
-const SHOW_REASONING = false;
+const SHOW_REASONING = true;
 
 // 🔥 THINKING MODE TOGGLE
-const ENABLE_THINKING_MODE = false;
+const ENABLE_THINKING_MODE = true;
 
 // 🔍 Set to true to see which model is actually respondin
 const SHOW_MODEL_TAG = true;
